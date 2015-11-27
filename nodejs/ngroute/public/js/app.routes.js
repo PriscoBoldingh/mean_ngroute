36f3405071a5fafd435eacb7a92e62ee13bdb64c
wrbrouwer <Richard.Brouwer@zonnet.nl>
2015-11-27 19:29:56 +0100

    Nog een aanpassing

--- nodejs/ngroute/public/js/app.routes.js
+++ nodejs/ngroute/public/js/app.routes.js
@@ -21,15 +21,7 @@ angular.module('routerRoutes', ['ngRoute'])
                 controllerAs: 'contact'
             });
 
-
-
-
-
-        
-
         // set our app up to have pretty URLS
         $locationProvider.html5Mode(true);
-
-
-
+        
     });
