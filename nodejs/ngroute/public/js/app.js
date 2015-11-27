59cba1e24cbb00b513b2ca49e312e9a8af3166c6
TjiepTjiep <prisco@PBMBA.local>
2015-11-27 17:34:23 +0100

    Initial commit

---
+++ nodejs/ngroute/public/js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by prisco on 24/11/15.
+ */
+angular.module('routerApp', ['routerRoutes'])
+
+//angular.module('routerApp',[])
+
+    .controller('mainController', function() {
+        var vm = this;
+        vm.bigMessage = 'a smooth sea never made a skilled sailor.';
+    })
+
+    .controller('homeController', function() {
+        var vm = this;
+        vm.message = 'This is the home page!';
+    })
+
+    .controller('aboutController', function() {
+        var vm = this;
+        vm.message = 'Look! I am an about page.';
+    })
+
+    .controller('contactController', function() {
+        var vm = this;
+        vm.message = 'Contact us! JK. This is just a demo.';
+    });
+
